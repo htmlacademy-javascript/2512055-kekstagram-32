818167674b3182b3b8cba7abe8c34379670e46cf
krupe <krupenkin2004@gmail.com>
2024-07-12 20:20:18 +0300

    module5-task1

---
+++ js/util.js
@@ -0,0 +1,22 @@
+function getRandomInteger(a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function getRandomArrayElement(items) {
+  return items[getRandomInteger(0, items.length - 1)];
+};
+
+function createIdGenerator() {
+  let numberId = 0;
+  return function() {
+    numberId += 1;
+    return numberId;
+  };
+};
+
+
+export {getRandomInteger, getRandomArrayElement,createIdGenerator};
+
