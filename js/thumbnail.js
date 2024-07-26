a2c0d470267680905cc0fbc71374042c2a7832ff
krupe <krupenkin2004@gmail.com>
2024-07-26 16:08:01 +0300

    module5-task1

---
+++ js/thumbnail.js
@@ -0,0 +1,26 @@
+const thumbnailTemplate = document.querySelector ('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+const createThumbnail = function (picture) {
+const thumbnail = thumbnailTemplate.cloneNode(true);
+
+thumbnail.querySelector('.picture_img').src = picture.url;
+thumbnail.querySelector('.picture_img').alt = picture.description;
+thumbnail.querySelector('.picture_likes').textContent = picture.likes;
+thumbnail.querySelector('.picture_comments').textContent = picture.comments.length;
+
+return thumbnail;
+};
+
+const generateThumbnails = function (pictures) {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach(function (picture) {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+
+
+container.append(fragment);
+};
+
+export {generateThumbnails};
